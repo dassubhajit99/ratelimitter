840e0b1c27403139e8eebc4e28ef9efa3bd6576a
dassubhajit99 <subhajit@candourtechnolabs.com>
2026-07-13 21:19:20 +0530

    implement deny limiter and set up HTTP server with rate limiting

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/dassubhajit99/ratelimitter.git/internal/limiter"
+	"github.com/dassubhajit99/ratelimitter.git/internal/server"
+)
+
+type denyLimiter struct {
+}
+
+func (denyLimiter) Allow(ctx context.Context, key string) (limiter.Result, error) {
+	return limiter.Result{
+		Allowed:    false,
+		Remaining:  0,
+		RetryAfter: 5 * time.Second,
+	}, nil
+}
 
 func main() {
-	fmt.Println("Hallo")
+	// rateLimiter := limiter.NoopLimiter{}
+	rateLimiter := denyLimiter{}
+
+	router := server.NewRouter(rateLimiter)
+
+	httpServer := &http.Server{
+		Addr:              ":8080",
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+
+	log.Printf("server listening on http://localhost%s", httpServer.Addr)
+
+	if err := httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("server failed :%v", err)
+	}
 }
